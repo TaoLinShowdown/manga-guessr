1d96f55dff6075f6440d7d64cc44ddd66d50b086
Tao Lin <47548247+TaoLinShowdown@users.noreply.github.com>
2022-01-19 01:29:35 -0500

    Add a version of game history and a reset button during the game

--- components/GameComponent.js
+++ components/GameComponent.js
@@ -97,17 +97,10 @@ export default function GameComponent({ mangas, titles, multipleChoice, resetGam
                     }
                 </div>
                 <div className={styles['game-side']}>
-                    <div className={styles['game-overlay']}>
+                    <div className={multipleChoice ? styles['game-overlay'] : `${styles['game-overlay']} ${styles['game-overlay-autocomplete']}`}>
                         <h3>
-                            <div className={styles['logo']}>
-                                <Image 
-                                    src='/mangaquizlogo_light.svg' 
-                                    alt='Logo goes here'
-                                    layout='fixed'
-                                    width={35}
-                                    height={35}
-                                />
-                                <span>MangaGuessr</span>
+                            <div className={styles['reset']}>
+                                <span onClick={resetGame}>Reset</span>
                             </div>
                             <div className={styles['roundscore']}>
                                 <span>
@@ -135,11 +128,23 @@ export default function GameComponent({ mangas, titles, multipleChoice, resetGam
                             />
                         }
                     </div>
-                    {/* <div className={styles['history']}>
-                        {mangas.filter((m, index) => index < currentRound).map(m =>
-                            <div>{m.titles[0]}</div>
-                        )}
-                    </div> */}
+                    <div className={styles['history']}>
+                        <div className={styles['history-logo']}>
+                            <img 
+                                src='/mangaquizlogo_light.svg' 
+                                alt='Logo goes here'
+                            />
+                        </div>
+                        <div className={styles['history-previews']}>
+                            {mangas.filter((m, index) => index < currentRound).map((m, index, mangas) =>
+                                <PreviewCard
+                                    mangaRef={mangas[mangas.length - 1 - index].ref}
+                                    title={`${mangas.length - index}. ${mangas[mangas.length - 1 - index].titles[0]}`}
+                                    pageLink={pageLinks[mangas.length - index - 1]}
+                                />
+                            )}
+                        </div>
+                    </div>
                 </div>
             </div>
         )
@@ -162,13 +167,26 @@ export default function GameComponent({ mangas, titles, multipleChoice, resetGam
                 </div>
                 <div className={styles['previews']}>
                     {mangas && mangas.map((m, index) => 
-                        <div key={index} className={styles['preview']}>
-                            <img src={pageLinks[index]}></img>
-                            <a href={`https://mangadex.org/title/${m.ref}`} target="_blank" rel="noopener noreferrer">{m.titles[0]} <svg data-v-20f285ec="" data-v-e3b182be="" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" ><path data-v-20f285ec="" d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6M15 3h6v6M10 14 21 3" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></svg></a>
-                        </div>
+                        <PreviewCard 
+                            mangaRef={m.ref}
+                            title={m.titles[0]}
+                            pageLink={pageLinks[index]}
+                        />
                     )}
                 </div>
             </div> 
         )
     }
+}
+
+function PreviewCard({ mangaRef, title, pageLink }) {
+    return (
+        <div className={styles['preview']}>
+            <img src={pageLink}></img>
+            <a href={`https://mangadex.org/title/${mangaRef}`} target="_blank" rel="noopener noreferrer">
+                {title}
+                <svg data-v-20f285ec="" data-v-e3b182be="" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" ><path data-v-20f285ec="" d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6M15 3h6v6M10 14 21 3" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></svg>
+            </a>
+        </div>
+    )
 }
